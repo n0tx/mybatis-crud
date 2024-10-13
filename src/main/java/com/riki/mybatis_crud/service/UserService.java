0554e862c127a84a962a348f9247a363dbfaa72e
n0tx <rcandra91@msn.com>
2024-10-13 17:32:39 +0700

    add user service, UserService.java

---
+++ src/main/java/com/riki/mybatis_crud/service/UserService.java
@@ -0,0 +1,35 @@
+package com.riki.mybatis_crud.service;
+
+import com.riki.mybatis_crud.mapper.UserMapper;
+import com.riki.mybatis_crud.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<User> getAllUsers() {
+        return userMapper.getAllUsers();
+    }
+
+    public User getUserById(int id) {
+        return userMapper.getUserById(id);
+    }
+
+    public void addUser(User user) {
+        userMapper.insertUser(user);
+    }
+
+    public void updateUser(User user) {
+        userMapper.updateUser(user);
+    }
+
+    public void deleteUser(int id) {
+        userMapper.deleteUser(id);
+    }
+}
