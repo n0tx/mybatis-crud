7770f88bba5cee666276c1f22a0284b86e8ec8c3
n0tx <rcandra91@msn.com>
2024-10-13 17:20:30 +0700

    Initialize commit MyBatis Crud

---
+++ src/test/java/com/riki/mybatis_crud/MybatisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.mybatis_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
